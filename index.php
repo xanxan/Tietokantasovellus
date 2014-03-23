a75541125dd7c63fb1c57318708afbdd50dd9c85
xanxan <anna.emilia.salo@gmail.com>
2014-03-23 12:25:55 +0200

    tiedostot siirretty korvaavaan repositorioon

---
+++ index.php
@@ -0,0 +1,62 @@
+Tietokantasovellus –harjoitustyö<!DOCTYPE html>
+<html>
+  <head>
+    <title>Tietokantasovellus</title>
+    <meta charset="utf-8" />
+    <style>
+      h1 { font-size: 1.4em; }
+      h2 { font-size: 1.1em; }
+    </style>
+  </head>
+  <body>
+    <h1>Tietokantasovelluksen esittelysivu</h1>
+    <p>Yleisiä linkkejä</p>
+    <ul>
+      <!-- Korvaa ohessa olevat sanat GITHUBTUNNUS ja 
+           REPOSITORIO omilla tiedoillasi: -->
+      <li><a href="https://github.com/xanxan/Tietokantasovellus-k2014">Työn repositorio</a></li>
+      <li><a href="https://github.com/xanxan/Tietokantasovellus-k2014/blob/master/doc/Dokumentaatio.pdf?raw=true">Työn dokumentaatio</a></li>
+
+      <!-- Viikolla 2 voit laittaa kommentoidut linkitkin sivuille. 
+           Ota kommenttimerkit pois sen kielen ympäriltä, jota käytät -->
+
+      <!-- Linkit Java-kielelle.
+           Korvaa sana cstunnus omalla tktl-tunnuksellasi ja sana 
+           Tietokantasovellus tietokantasovelluksesi usersille 
+           pystytetyllä nimellä: 
+      -->
+      <!--
+      <li><a href="http://t-cstunnus.users.cs.helsinki.fi/ConnectionTest">Connectiontest-ohjelma</a></li>
+      <li><a href="http://t-cstunnus.users.cs.helsinki.fi/Tietokantasovellus/html-demo/index.jsp">HTML-demosivujen etusivu</a></li>
+      <li><a href="http://t-cstunnus.users.cs.helsinki.fi/Tietokantasovellus/Listaustesti">Ensimmäinen oma tietokantalistaus</a></li>
+      -->
+      <!-- Linkit PHP-kielelle.
+           Korvaa sana cstunnus omalla tktl-tunnuksellasi ja sana 
+           Tietokantasovellus sen hakemiston nimellä, missä 
+           sovelluksesi usersin htdocs-kansiossa sijaitsee:
+      -->
+      <!--
+      <li><a href="http://cstunnus.users.cs.helsinki.fi/connectiontest.php">Connectiontest-ohjelma</a></li>
+      <li><a href="http://cstunnus.users.cs.helsinki.fi/Tietokantasovellus/html-demo/index.html">HTML-demosivujen etusivu</a></li>
+      <li><a href="http://cstunnus.users.cs.helsinki.fi/Tietokantasovellus/listaustesti.php">Ensimmäinen oma tietokantalistaus</a></li>
+      -->
+      
+      <!-- Viikolla 3 laita alla kommentoitu linkki osoittamaan omaan kirjautumissivuusi. -->
+      <!--
+      <li><a href="http://cstunnus.users.cs.helsinki.fi/Tietokantasovellus/index.php">Sovelluksen kirjautumissivu</a></li>
+      -->
+    </ul>
+
+    <h2>Työn aihe</h2>
+     Tietokantasovelluksena toteutetaan www-sivuilla toimiva ravintolatietokanta.  Ravintolatietokannan käyttäjät voivat etsiä sovelluksella mieleistään ravintolaa Helsingin alueelta ja selailla listaa ravintoloista. 
+
+    <!--
+    <h2>Kirjautumistunnuksia testausta varten</h2>
+
+    <p>Laita tähän viikolla 3 lista tunnuksista, joilla sovellusta voi testata</p>
+    <p>Esim:<br />
+    Pääkäyttäjän tunnus ja salasana:<br /> admin, kissakala
+    </p>
+    -->
+  </body>
+</html>

4d6f645ef691e1931630a9f93fc1323e3686875d
xanxan <anna.emilia.salo@gmail.com>
2014-03-23 23:24:57 +0200

    sql-lisäykset ja testit tehty

--- sql/add-test-data.sql
+++ sql/add-test-data.sql
@@ -1,1 +1,3 @@
-moimoi:)
+insert into kayttajat values(0,0,0,'https://www.dropbox.com/s/sx7s3cv6gz4hs6j/Historia8.jpg','Kohta 20v käpistelijätyttö Helsingistä','23.03.2014','minni',0,'framboise',3493588);
+
+insert into ravintolat values(76869,'Ma-Pe klo 10-21, La klo 12-20',0,'€€',0,0,'http://xanxan.users.cs.helsinki.fi/html-demo/Esimravintolakuva.jpg','Perinteinen amerikkalainen hampurilaisravintola','Burger King','WhopperLane 56, 004590 Helsinki',0,'pikaruokaravintola');
--- sql/drop-tables.sql
+++ sql/drop-tables.sql
@@ -1,1 +1,8 @@
-moi:)
+DROP TABLE kommentit;
+DROP TABLE yllapitajat;
+DROP TABLE kayttajat;
+DROP TABLE inhokkilista;
+DROP TABLE suosikkilista;
+DROP TABLE sopivuustiedot;
+DROP TABLE ravintolat;
+DROP TABLE arvostelut;
